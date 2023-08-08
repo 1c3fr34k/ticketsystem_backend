cbd3c8ef8cb202ec69252140b6f2a494cea76e9d
1c3fr34k <56366285+1c3fr34k@users.noreply.github.com>
2023-08-08 22:28:40 +0200

    added swagger and cors

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/1c3fr34k/ticketsystem_backend/database"
@@ -11,6 +12,10 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	_ "github.com/1c3fr34k/ticketsystem_backend/docs"
+	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
 var (
@@ -38,6 +43,20 @@ func init() {
 	dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", env_host, env_user, env_password, env_database, env_port, env_sslmode)
 }
 
+//	@title			Swagger Example API
+//	@version		1.0
+//	@description	This is a sample server Petstore server.
+//	@termsOfService	http://swagger.io/terms/
+
+//	@contact.name	API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
+
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host		petstore.swagger.io
+// @BasePath	/v2
 func main() {
 
 	e := echo.New()
@@ -49,7 +68,15 @@ func main() {
 
 	e.Use(middlewares.ContextDB(db))
 
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"*"},
+		AllowHeaders:     []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete, echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowCredentials: true,
+		ExposeHeaders:    []string{"Access-Control-Allow-Origin"},
+	}))
+
 	routes.Routes(e.Group("/api"))
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
 	data, err := json.MarshalIndent(e.Routes(), "", "  ")
 	if err != nil {
