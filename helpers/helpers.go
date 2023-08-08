cc5c02f061a6b19deea84b0a3c317fd54f7a5218
1c3fr34k <56366285+1c3fr34k@users.noreply.github.com>
2023-08-08 22:30:15 +0200

    added password hash helper function

---
+++ helpers/helpers.go
@@ -0,0 +1,25 @@
+package helpers
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	// fmt.Println(password)
+	// fmt.Println(hashed)
+	// fmt.Println(string(hashed))
+
+	return string(hashed), nil
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return false
+	}
+	return true
+}
